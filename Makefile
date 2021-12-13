fe2258f81511d68b67c3d671b28d065332edb183
progs2002 <pragatobhattacharjee@gmail.com>
2021-12-13 22:28:01 +0530

    started with python

--- Makefile
+++
@@ -1,7 +0,0 @@
-# -*- MakeFile -*-
-all:
-	g++ src/*.cpp -I. -Iinclude -o build/app
-
-build:
-	g++ src/*.cpp -I. -Iinclude -o build/app
-	./build/app
--- include/train.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define LOG(x) std::cout << x << std::endl
-#include <iostream>
-#include <cstring>
-
-int train_init(const char*);
-int train();
-int cleanup();
